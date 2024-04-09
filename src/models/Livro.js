caca9423ce4ff9dc2283619e1f6d7081b006ae03
Cristhian Cintra <64995266+CintraB@users.noreply.github.com>
2024-04-08 23:03:32 -0300

    Adição validador global
    foi adicionado validador global verificando campos em branco.

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -4,11 +4,23 @@ import { autorSchema } from "./Autor.js";
 
 const livrosSChema = new mongoose.Schema({
   id: { type: mongoose.Schema.Types.ObjectId },
-  titulo: { type: String, 
-    required: [true, "O titulo do livro é obrigatório."] },
+
+  titulo: { type: String, required: [true, "O titulo do livro é obrigatório"] },
+
   editora: { type: String },
+
   preco: { type: Number },
-  paginas: { type: Number },
+
+  paginas: {
+    type: Number,
+    validate: {
+      validator: (valor) => {
+        return valor >= 10 && valor <= 5000;
+      },
+      message: "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+    }
+  },
+
   autor: autorSchema
 }, { versionKey: false });
 
